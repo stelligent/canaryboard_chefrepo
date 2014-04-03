9820cc603070f6399ceaecade06ef1e709e04568
Jonathan Sywulak <jonathan.sywulak@stelligent.com>
2014-04-03 16:48:36 -0400

    let's get crazy

--- canaryboard/recipes/dbsetup.rb
+++ canaryboard/recipes/dbsetup.rb
@@ -2,6 +2,10 @@ execute "rake db:setup" do
   cwd "/srv/www/canaryboard/current"
 end
 
+execute "rake db:migrate" do
+  cwd "/srv/www/canaryboard/current"
+end
+
 execute "rake db:test:prepare" do
   cwd "/srv/www/canaryboard/current"
 end
